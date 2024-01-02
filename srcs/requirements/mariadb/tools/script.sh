93dbbe4e4d77a1ce11f44595650504bbf963c4d9
Max Schaub <mars12161@gmail.com>
2024-01-02 15:20:37 +0100

    [inception] mariadb container is done

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 
-if [! -d "/run/mysqld" ]; then
+if [ ! -d "/run/mysqld" ]; then
 	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysql
+	chown -R mysql:mysql /run/mysqld
 fi
 
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+	tempfile=$(mktemp)
+	if [ ! -f "$tempfile" ]; then
+		return 1
+	fi
+
+	cat << EOF > "$tempfile"
+	
+USE mysql;
+FLUSH PRIVILEGES;
+
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.db WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$rootpw';
+CREATE DATABASE '$dbname' CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$username'@'%' IDENTIFIED BY '$dbpasswd';
+GRANT ALL PRIVILEGES ON $dbname.* TO '$username'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+	/usr/sbin/mysqld --user=mysql --bootstrap < "$tempfile"
+	rm -f "$tempfile"
+
+fi
+
+sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+exec /usr/sbin/mysqld --user=mysql --console
