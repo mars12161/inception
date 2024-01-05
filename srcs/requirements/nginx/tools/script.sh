1513a542ca2febc90d0de64a12c424d467bb4c34
Max Schaub <mars12161@gmail.com>
2024-01-05 15:55:25 +0100

    [inception] small improvement but not properly working yet

--- srcs/requirements/nginx/tools/script.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-	echo "Setting up SSL";
-	openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/max.key -out /etc/ssl/certs/nginx.crt \
-	-subj "/C=DE/ST=Berlin/L=Berlin/O=42B/CN=max";
-fi;
-
-exec "$@"
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
-chown -R www-data:www-data /var/www/*;
-chown -R 755 /var/www/*;
+chown -R www-data /var/www/wordpress
+chmod -R 755 /var/www/wordpress;
+
 mkdir -p /run/php/;
 touch /run/php/php7.3-fpm.pid;
 
-if [ ! -f /var/www/html/wp-config.php ]; then
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	echo "Setting up wordpress"
-	mkdir -p /var/www/html
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
 	chmod +x wp-cli.phar;
 	mv wp-cli.phar /usr/local/bin/wp;
-	cd /var/www/html;
+
+	cd /var/www/wordpress;
 	wp core download --allow-root;
-	mv /var/www/cp-config.php /var/www/html/
+
+	mv /var/www/cp-config.php /var/www/wordpress/
 	echo "Creating wordpress admin and user"
 	wp core install --allow-root --url=${URL} --title${TITLE} --admin_user${WP_ADMIN} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_ADMIN_MAIL};
 	wp user create --allow-root ${WP_USER} ${WP_USER_MAIL} --user-pass=${WP_USER_PW};
 
 fi
 
-exec "$@"
+/usr/sbin/php-fpm7.3 -F
