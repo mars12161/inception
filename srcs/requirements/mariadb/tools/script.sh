ee7ada733b2a15202fd15d8bcee574009d31a9c9
Max Schaub <mars12161@gmail.com>
2024-01-16 16:45:39 +0100

    wordpress is properly installed

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+
+sleep 5;
+
 service mysql start
 
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
-mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -uroot -p"rootpass" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpass';"
+mysql -uroot -p"rootpass" -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mysql -uroot -p"rootpass" -e "CREATE USER IF NOT EXISTS 'lituser'@'localhost' IDENTIFIED BY 'litpw';"
+mysql -uroot -p"rootpass" -e "GRANT ALL PRIVILEGES ON *.* TO 'lituser'@'%' IDENTIFIED BY 'litpw';"
+#mysql -uroot -p"rootpass" -e "GRANT ALL PRIVILEGES ON *.* TO 'lituser'@'localhost' WITH GRANT OPTION;"
+mysql -uroot -p"rootpass" -e "FLUSH PRIVILEGES;"
 
 exec "$@"
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
 cd /var/www/wordpress
-
-wp core config --dbhost=$MYSQL_HOSTNAME --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER \
+	
+wp core config --dbhost=mariadb --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER \
 	--dbpass=$MYSQL_PASSWORD --allow-root
 
-wp core install --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS \
-	--admin_email=$WP_MAIL --url=${WP_URL} --allow-root
+wp core install --title=inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS \
+	--admin_email=$WP_MAIL --url=$WP_URL --allow-root
 
-wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASS --allow-root
+wp user create $MYSQL_USER $WP_USER_MAIL --role=author --user_pass=$MYSQL_PASSWORD --allow-root
 
 cd -
-
-echo "Current directory: $(pwd)"
-ls -l /var/www/wordpress
 
 /usr/sbin/php-fpm7.3 -F
 
