872fd5d13633c5109e74ca1e6c7a5f92808d44d9
Max Schaub <mars12161@gmail.com>
2024-01-17 12:23:35 +0100

    fully working

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -5,11 +5,10 @@ sleep 5;
 
 service mysql start
 
-mysql -uroot -p"rootpass" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpass';"
-mysql -uroot -p"rootpass" -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-mysql -uroot -p"rootpass" -e "CREATE USER IF NOT EXISTS 'lituser'@'localhost' IDENTIFIED BY 'litpw';"
-mysql -uroot -p"rootpass" -e "GRANT ALL PRIVILEGES ON *.* TO 'lituser'@'%' IDENTIFIED BY 'litpw';"
-#mysql -uroot -p"rootpass" -e "GRANT ALL PRIVILEGES ON *.* TO 'lituser'@'localhost' WITH GRANT OPTION;"
+mysql -uroot -p"rootpass" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -uroot -p"rootpass" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -uroot -p"rootpass" -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -uroot -p"rootpass" -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -uroot -p"rootpass" -e "FLUSH PRIVILEGES;"
 
 exec "$@"
